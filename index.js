b3b569f6f3bee992955ef25da730ac3c52d75dfd
Marina Kukso <marina.kukso@gmail.com>
2017-07-13 21:46:55 -1000

    2 different draw fn's - one red, one normal'd. next step: render to framebuffer

--- index.js
+++ index.js
@@ -4,46 +4,58 @@ var camera = require('regl-camera')(regl,{
 })
 var anormals = require('angle-normals')
 var mat4 = require('gl-mat4')
-
-function catmug (regl) {
-  var catmug = require('./libraries/catmug.json')
-  var model = []
-  return regl({
+var catmug = require('./libraries/catmug.json')
+var model = []
+var catopts = { 
+  vert: `
+    precision mediump float;
+    uniform mat4 projection, view, model;
+    attribute vec3 position, normal;
+    varying vec3 vnorm, vpos;
+    void main (){
+      vnorm = normal;
+      vpos = position;
+      gl_Position = projection * view * model *
+      vec4(position, 1);
+    }
+  `,
+  attributes: {
+    position: catmug.positions,
+    normal: anormals(catmug.cells, catmug.positions)
+  },
+  uniforms: {
+    model: function (context){
+      return mat4.identity(model)
+    },
+    time: regl.context('time')
+  },
+  primitive: "triangles",
+  elements: catmug.cells
+}
+function catmugshow (regl) {
+  return regl(Object.assign({
     frag: `
       precision mediump float;
       varying vec3 vnorm, vpos;
       void main (){
         gl_FragColor = vec4(vnorm - vpos,1);
       }
-    `,
-    vert: `
+    `
+  }, catopts))
+}
+function catmugpick (regl) {
+  return regl(Object.assign({
+    frag: `
       precision mediump float;
-      uniform mat4 projection, view, model;
-      attribute vec3 position, normal;
       varying vec3 vnorm, vpos;
       void main (){
-        vnorm = normal;
-        vpos = position;
-        gl_Position = projection * view * model *
-        vec4(position, 1);
+        gl_FragColor = vec4(1,0,0,1);
       }
-    `,
-    attributes: {
-      position: catmug.positions,
-      normal: anormals(catmug.cells, catmug.positions)
-    },
-    uniforms: {
-      model: function (context){
-        return mat4.identity(model)
-      },
-      time: regl.context('time')
-    },
-    primitive: "triangles",
-    elements: catmug.cells
-  })
+    `
+  }, catopts))
 }
 var draw = {
-  catmug : catmug(regl)
+  catmug : catmugshow(regl)
 }
 regl.frame(function(context){
   regl.clear({color: [0,0,0,1], depth:true})
